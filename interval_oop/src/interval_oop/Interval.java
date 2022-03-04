1b395460a5a87acf2419227cde63ea93191bc916
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2022-03-04 12:25:08 +0100

    Implementation using fields lowerBound and upperBound

--- interval_oop/src/interval_oop/Interval.java
+++ interval_oop/src/interval_oop/Interval.java
@@ -10,9 +10,17 @@ package interval_oop;
  */
 public class Interval {
 	
-	public int getLowerBound() { throw new RuntimeException("Not yet implemented"); }
-	public int getUpperBound() { throw new RuntimeException("Not yet implemented"); }
-	public int getLength() { throw new RuntimeException("Not yet implemented"); }
+	/**
+	 * Representation invariants:
+	 * 
+	 * @invar | lowerBound <= upperBound
+	 */
+	private int lowerBound;
+	private int upperBound;
+	
+	public int getLowerBound() { return lowerBound; }
+	public int getUpperBound() { return upperBound; }
+	public int getLength() { return upperBound - lowerBound; }
 	
 	/**
 	 * Initializes this object to store the interval with the given lower bound and upper bound.
@@ -22,7 +30,13 @@ public class Interval {
 	 * @post | getLowerBound() == lowerBound
 	 * @post | getUpperBound() == upperBound
 	 */
-	public Interval(int lowerBound, int upperBound) { throw new RuntimeException("Not yet implemented"); }
+	public Interval(int lowerBound, int upperBound) {
+		if (upperBound < lowerBound)
+			throw new IllegalArgumentException("`upperBound` is less than `lowerBound`");
+		
+		this.lowerBound = lowerBound;
+		this.upperBound = upperBound;
+	}
 	
 	/**
 	 * Shifts the interval stored by this object to the right by the given amount.
@@ -30,6 +44,9 @@ public class Interval {
 	 * @post | getLowerBound() == old(getLowerBound()) + amount
 	 * @post | getUpperBound() == old(getUpperBound()) + amount
 	 */
-	public void shift(int amount) { throw new RuntimeException("Not yet implemented"); }
+	public void shift(int amount) {
+		this.lowerBound += amount;
+		this.upperBound += amount;
+	}
 
 }
